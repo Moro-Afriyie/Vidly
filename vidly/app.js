b8c71446a848ee472a6c243a2c890f7e6839a6db
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-03 15:16:18 -0100

    add the connection to mongodb

--- vidly/app.js
+++ vidly/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 const genres = require("./routes/genres");
+const mongoose = require("mongoose");
 PORT = 8000;
 
+mongoose
+  .connect("mongodb://localhost:27017", {
+    useUnifiedTopology: true,
+  })
+  .then((res) => console.log("connected to mongoDB"))
+  .catch((err) => console.log("unable to connect to mongoDB"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
