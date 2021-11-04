67279dadf08681f1fc742ad34de5e528532da9d9
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-04 10:52:30 -0100

    done implementing the movielists

--- vidly/routes/movie.js
+++ vidly/routes/movie.js
@@ -13,46 +13,54 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const genre = await Movie.findById(req.params.id);
-    if (!genre) {
-      return res.status(404).send("The genre you entered does not exist ");
+    const movie = await Movie.findById(req.params.id);
+    if (!movie) {
+      return res.status(404).send("The movie you entered does not exist ");
     }
-    res.send(genre);
+    res.send(movie);
   } catch (error) {
-    res.status(404).send("The genre you entered does not exist ");
+    res.status(404).send("The movie you entered does not exist ");
   }
 });
 
 router.post("/", async (req, res) => {
-  const { error } = validateMovie(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-  let genre = new Movie({ name: req.body.name });
-  genre = await genre.save();
-  res.send(genre);
+  //   const { error } = validateMovie(req.body);
+  //   if (error) {
+  //     return res.status(400).send(error.details[0].message);
+  //   }
+  let movie = new Movie({
+    title: req.body.title,
+    genre: { name: req.body.genre },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate,
+  });
+  movie = await movie.save();
+  res.send(movie);
 });
 
 router.put("/:id", async (req, res) => {
-  const genre = await Movie.findByIdAndUpdate(
+  const movie = await Movie.findByIdAndUpdate(
     req.params.id,
     {
-      name: req.body.name,
+      title: req.body.title,
+      genre: { name: req.body.genre },
+      numberInStock: req.body.numberInStock,
+      dailyRentalRate: req.body.dailyRentalRate,
     },
     { new: true }
   );
-  if (!genre) {
-    return res.status(404).send("The genre you entered does not exist ");
+  if (!movie) {
+    return res.status(404).send("The movie you entered does not exist ");
   }
-  res.status(200).send(genre);
+  res.status(200).send(movie);
 });
 
 router.delete("/:id", async (req, res) => {
-  const genre = await Movie.findByIdAndRemove(req.params.id);
-  if (!genre) {
-    return res.status(404).send("The genre you entered does not exist ");
+  const movie = await Movie.findByIdAndRemove(req.params.id);
+  if (!movie) {
+    return res.status(404).send("The movie you entered does not exist ");
   }
-  res.status(200).send("genre deleted succesfully");
+  res.status(200).send("movie deleted succesfully");
 });
 
 module.exports = router;
