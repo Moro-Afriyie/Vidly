e676d64b501dc2e2c89436e19ceb217cd8a840d3
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-04 11:11:56 -0100

    updated the validations

--- vidly/models/movieModel.js
+++ vidly/models/movieModel.js
@@ -5,10 +5,10 @@ const { genresSchema } = require("./genresModel");
 const Movie = mongoose.model(
   "Movies",
   new mongoose.Schema({
-    title: { type: String, required: true },
+    title: { type: String, required: true, trim: true },
     genre: { type: genresSchema, required: true },
-    numberInStock: { type: Number, required: true, default: 0 },
-    dailyRentalRate: { type: Number, required: true, default: 0 },
+    numberInStock: { type: Number, required: true, default: 0, min: 0 },
+    dailyRentalRate: { type: Number, required: true, default: 0, min: 0 },
   })
 );
 
@@ -16,8 +16,8 @@ const validateMovie = (movie) => {
   const schema = Joi.object({
     title: Joi.string().min(3).max(255).required(),
     genre: Joi.string().min(3).max(255).required(),
-    numberInStock: Joi.number(),
-    dailyRentalRate: Joi.number(),
+    numberInStock: Joi.number().min(0),
+    dailyRentalRate: Joi.number().min(0),
   });
 
   return schema.validate(movie);
