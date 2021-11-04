df81ad36566f07844e762e78418be9b77ab5e97f
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-04 07:38:07 -0100

    create customers model

---
+++ vidly/models/customersModels.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema({
+  isGold: { type: Boolean, required: true },
+  name: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+});
+
+const Customer = mongoose.model("Customers", customerSchema);
+
+module.exports = Customer;
