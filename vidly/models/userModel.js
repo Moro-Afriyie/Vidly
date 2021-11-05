9a5990121ce03eac66056a2ed36fe6ee49cf053a
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-05 10:38:08 -0100

    update the userModel

--- vidly/models/userModel.js
+++ vidly/models/userModel.js
@@ -4,20 +4,20 @@ const Joi = require("joi");
 const User = mongoose.model(
   "User",
   new mongoose.Schema({
-    name: { type: String, required: true, trim: true, min: 5, max: 255 },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    name: { type: String, required: true, trim: true, min: 5, max: 50 },
+    email: { type: String, required: true, unique: true, min: 5, max: 255 },
+    password: { type: String, required: true, min: 8, max: 1024 },
   })
 );
 
 const validateUser = (user) => {
   const schema = Joi.object({
-    name: Joi.string().min(5).max(255).required(),
+    name: Joi.string().min(5).max(50).required(),
     email: Joi.string().email({
       minDomainSegments: 2,
       tlds: { allow: ["com", "net"] },
     }),
-    password: Joi.string().min(8).max(255).required(),
+    password: Joi.string().min(8).max(1024).required(),
   });
 
   return schema.validate(user);
