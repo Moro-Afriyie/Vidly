10fa1bad3d0abb4d4be795b23b23e812a8af47f2
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-05 13:54:05 -0100

    create a generateAuthToken method on userSchema to generate tokens

--- vidly/models/userModel.js
+++ vidly/models/userModel.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 
-const User = mongoose.model(
-  "User",
-  new mongoose.Schema({
-    name: { type: String, required: true, trim: true, min: 5, max: 50 },
-    email: { type: String, required: true, unique: true, min: 5, max: 255 },
-    password: { type: String, required: true, min: 8, max: 1024 },
-  })
-);
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true, min: 5, max: 50 },
+  email: { type: String, required: true, unique: true, min: 5, max: 255 },
+  password: { type: String, required: true, min: 8, max: 1024 },
+});
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, "jwtPrivateKey");
+  return token;
+};
+
+const User = mongoose.model("User", userSchema);
 
 const validateUser = (user) => {
   const schema = Joi.object({
