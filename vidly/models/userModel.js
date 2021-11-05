395b544c73d0293b5d9211a5c5cc2d9fbb4d5933
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-05 09:57:55 -0100

    create a userModel.js file

---
+++ vidly/models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  })
+);
+
+const validateUser = (user) => {
+  const schema = Joi.Object({
+    name: Joi.string().min(5).max(255).required(),
+    email: Joi.string().email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    }),
+    password: Joi.string().min(8).max(255).required(),
+  });
+
+  return schema.validate(user);
+};
+
+module.exports = { User };
