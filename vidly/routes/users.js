c8579676c9f80208fb50092dc2065df9a6f07d7a
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-05 10:05:31 -0100

    create the user route endpoint

---
+++ vidly/routes/users.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const {User, validateUser} = require("../models/userModel");
+const router = express.Router();
+
+router.post("/", async (req, res)=>{
+
+});
+
+module.exports = router;
