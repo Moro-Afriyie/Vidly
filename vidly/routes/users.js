88777e3be1bdbc0311750e563b73bb0681d47feb
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-05 10:08:16 -0100

    implement the user route and database

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -1,9 +1,20 @@
-const express = require('express')
-const {User, validateUser} = require("../models/userModel");
+const express = require("express");
+const { User, validateUser } = require("../models/userModel");
 const router = express.Router();
 
-router.post("/", async (req, res)=>{
+router.post("/", async (req, res) => {
+  const { error } = validateUser(req.body);
 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  await user.save();
+  res.send(user);
 });
 
 module.exports = router;
